4f4ad8959792b56e132dc3f3a51903a2e8d20f58
flamozzi <pjs99047@gmail.com>
2022-04-14 16:03:46 +0900

    Initial Commit

---
+++ Paperful/PaperfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperfulApp.swift
+//  Paperful
+//
+//  Created by Tony on 2022/04/14.
+//
+
+import SwiftUI
+
+@main
+struct PaperfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
